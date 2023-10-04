c5de8f21bca82ade82bbff561af01e572af7c766
Anton Melnikov <101597291+Melnikov-A@users.noreply.github.com>
2023-10-04 11:56:37 +0300

    added getProperty cfg

--- src/test/java/guru/qa/TestBase.java
+++ src/test/java/guru/qa/TestBase.java
@@ -22,7 +22,7 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        Configuration.remote = System.getProperty("remoteDriver");
+        Configuration.remote = System.getProperty("SelenoidUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
 
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
